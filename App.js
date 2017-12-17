2c8e2dc65c11cb1db735f16e067c6485872aadb9
Joshua <jmg1138@gmail.com>
2017-12-17 13:34:38 -0800

    Added some icons

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5bbad5',
     alignItems: 'center',
     justifyContent: 'center',
   },
